0d3c930139e22a85cf1c0ed91e54112f67f85041
parjolandrei <parjol.andrei@yahoo.com>
2020-03-09 13:00:02 +0200

    Updates

---
+++ precache-manifest.77e9f7de04dc80b5c53c1058a07e5b4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9901e76f5ceab1e41d97a36a233059",
+    "url": "/recipe-search/index.html"
+  },
+  {
+    "revision": "724d8e9e9af02935613b",
+    "url": "/recipe-search/static/css/main.97da3886.chunk.css"
+  },
+  {
+    "revision": "830fd1d6e6487f22ce45",
+    "url": "/recipe-search/static/js/2.d2977089.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/recipe-search/static/js/2.d2977089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "724d8e9e9af02935613b",
+    "url": "/recipe-search/static/js/main.9cf3151a.chunk.js"
+  },
+  {
+    "revision": "1d279cddf69f35900fd5",
+    "url": "/recipe-search/static/js/runtime-main.0fd3b477.js"
+  }
+]);
